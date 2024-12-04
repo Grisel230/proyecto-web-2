8db1e8bf0d533f9818b6431ed7123fbf2c704900
grisel23$ <grisellaurean@gmail.com>
2024-12-03 23:49:57 -0700

    login ya casi

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,34 @@
 import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  onLogin() {
+    this.authService.login(this.email, this.password ).subscribe({
+      next: (res: any) => {
+        localStorage.setItem('token', res.token);
+        alert('ola');
+        this.router.navigate(['/']);
+      },
+      error: (err) => alert('Credenciales incorrectas')
+    });
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,33 @@
 import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
 
+constructor(private authService: AuthService, private router: Router) {
+}
+  onRegister() {
+      this.authService.register(this.name, this.email, this.password).subscribe(response => {
+        console.log('Registration successful', response);
+        this.router.navigate(['/']);
+        // Handle successful registration
+      }, error => {
+        console.error('Registration failed', error);
+        // Handle registration error
+      });
+    }
 }
